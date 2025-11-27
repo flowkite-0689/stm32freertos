3d9f39edaec23b4fc804ec032417f508aa3cb53e
kes <2604338097@qq.com>
2025-11-27 10:44:43 +0800

    1

--- User/FreeRTOS/include/hooks.h
+++ User/FreeRTOS/include/hooks.h
@@ -1,15 +1,18 @@
 #ifndef _HOOKS_H_
 #define _HOOKS_H_
-\
 
 #include <stm32f4xx.h>
 #include <FreeRTOS.h>
 #include <task.h>
-void vApplicationIdleHook( void );
-void vApplicationStackOverflowHook( TaskHandle_t pxTask, char *pcTaskName );
-void vApplicationTickHook( void );
+void vApplicationIdleHook(void);
+void vApplicationStackOverflowHook(TaskHandle_t pxTask, char *pcTaskName);
+void vApplicationTickHook(void);
 
-void vApplicationMallocFailedHook( void );
-
-
+void vApplicationMallocFailedHook(void);
+void vApplicationGetIdleTaskMemory(StaticTask_t **ppxIdleTaskTCBBuffer,
+                                   StackType_t **ppxIdleTaskStackBuffer,
+                                   uint32_t *pulIdleTaskStackSize);
+void vApplicationGetTimerTaskMemory(StaticTask_t **ppxTimerTaskTCBBuffer,
+                                    StackType_t **ppxTimerTaskStackBuffer,
+                                    uint32_t *pulTimerTaskStackSize);
 #endif
